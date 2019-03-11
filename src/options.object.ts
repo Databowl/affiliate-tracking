bb083cc6af77613d53d2a5e9c6c4ccb818fa4ff5
Peter Millard <peter@mediabowl.co.uk>
2019-03-11 10:58:10 +0000

    SB2-942 - Ensured options object gets affiliate url from environment

--- src/options.object.ts
+++ src/options.object.ts
@@ -1,10 +1,12 @@
 import {defaultCookieExpiryInDays} from "./helpers/cookie.helper";
 
+import environment from "../environments/environment";
+
 export class OptionsObject {
     constructor(
         public urlId: string,
         public defaultAffiliateId: string,
-        public baseUrl: string = 'https://dbevt.com/',
+        public baseUrl: string = environment.affiliatesBaseUrl,
         public cookieExpiryInDays: number = defaultCookieExpiryInDays,
         public cookiePrefix: string = '',
         public cookiePath: string = '/',
